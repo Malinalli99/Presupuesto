a906a6227bf3c7b5c66a5fd6ae81387186cba647
Profesional <profesional@CISC2106D25QP03SGF1L.local>
2020-02-18 18:32:59 -0600

    puse un mensaje

---
+++ PresupuestaT/PresupuestaT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PresupuestaT
+//
+//  Created by Profesional on 2/18/20.
+//  Copyright © 2020 Profesional. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
